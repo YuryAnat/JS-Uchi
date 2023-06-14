90b5be35aaa2056395bf88ccfd92723f82271e1b
YAnatienko <YAnatienko@elar.ru>
2023-06-14 15:05:30 +0300

    task2

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log('hello ' + 'js');
+// console.log(111);
+// alert('alert');
+
+var a = 1; //устаревший
+let b = 2; //современный
+const con = 0; //константа
+
+console.log(con);
+
+console.log(temp);
+var temp = 't';
+
+let tempL = 'l';
+console.log(tempL);
+
+let i = Infinity;
+console.log(i);
+
+console.log(2.33 / 10);
+
+console.log('2k' / 2);
+
+console.log(99999999999999999n + 1n);
+
+let fistStr = 'one';
+let str2 = "two";
+let str3 = `three ${fistStr}`
+
+console.log(fistStr);
+console.log(str2);
+console.log(str3);
+
+let check = true;
+let unCheck = false;
+
+console.log(str2 == str3);
+
+console.log(typeof str2);
+console.log(typeof i);
+console.log(typeof null);
